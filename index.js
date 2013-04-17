866eae9f93f3c56733f5b1a716efc7e1755a44ec
Arvid Kahl <arvid@koding.com>
2013-04-16 17:04:32 -0700

    gitignore

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// Compiled by Koding Servers at Tue Apr 16 2013 16:43:43 GMT-0700 (PDT) in server time
+// Compiled by Koding Servers at Tue Apr 16 2013 17:02:14 GMT-0700 (PDT) in server time
 
 (function() {
 
